6e78d7b6e90b186e1d84b7401654f2ad487b1fa6
Brian Weenk <60255415+BrianWeenkGames@users.noreply.github.com>
2025-01-20 19:36:33 +0100

    Add files via upload
    Krijg een overzicht met voertuigen die aanrijdend en/of ter plaatse zijn bij/naar een melding. Dit overzicht komt onder de benodigde voertuigen

---
+++ Voertuigen ter plaatse-1.0.user.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name [MKS]Voertuigen ter plaatse
+// @namespace http://tampermonkey.net/
+// @version 1.0
+// @description try to take over the world!
+// @author BrianWeenk
+// @match https://www.meldkamerspel.com/missions/*
+// @require http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js
+// @downloadURL https://raw.githubusercontent.com/NHendriks01/LSS-MKS/main/Voertuigen_ter_plaatse.user.js
+// @updateURL https://raw.githubusercontent.com/NHendriks01/LSS-MKS/main/Voertuigen_ter_plaatse.user.js
+// ==/UserScript==
+
+var onSpot = $('#mission_vehicle_at_mission a.btn-backalarm-ajax').length;
+var driving = $('#mission_vehicle_driving td.hidden-xs div.btn-group a.btn').length;
+if(driving==1){
+$('.alert-missing-vehicles').after('<div class="alert alert-warning">Je hebt één voertuig onderweg naar deze melding</div>');
+}
+if(driving>1){
+$('.alert-missing-vehicles').after('<div class="alert alert-success">Je hebt '+driving+' voertuigen onderweg naar deze melding</div>');
+}
+if(onSpot==1){
+$('.alert-missing-vehicles').after('<div class="alert alert-warning">Je hebt één voertuig ter plaatse</div>');
+}
+if(onSpot>1){
+$('.alert-missing-vehicles').after('<div class="alert alert-success">Je hebt '+onSpot+' voertuigen ter plaatse</div>');
+}
+if(!onSpot && !driving){
+$('.alert-missing-vehicles').after('<div class="alert alert-danger">Je hebt nog geen voertuigen naar deze melding gestuurd</div>');
+}
