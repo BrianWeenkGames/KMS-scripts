c2d0c43617e3a6ca856a1c2c99f0ae9f24715c67
Brian Weenk <60255415+BrianWeenkGames@users.noreply.github.com>
2025-01-20 19:26:43 +0100

    Add files via upload
    De oranje voertuiglimietbalk al aanklikken

---
+++ voertuiglimitbalk (variabel balk)-1.0.user.js
@@ -0,0 +1,46 @@
+// ==UserScript==
+// @name         voertuiglimitbalk (variabel balk)
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  try to take over the world!
+// @author       brianweenk
+// @match        https://www.meldkamerspel.com/missions/*
+// @grant        none
+// ==/UserScript==
+ 
+(function() {
+
+// vul hier de aantal keer in dat de balk automatisch ingedrukt moet worden.
+
+    const numberOfTimes = 3;
+
+// hieronder niks meer aanpassen! anders werkt het script niet meer!!
+
+    let clickCount = 0; 
+    const observer = new MutationObserver((mutations) => {
+        mutations.forEach((mutation) => {
+            if (mutation.type === 'attributes' && (mutation.attributeName === 'class' || mutation.attributeName === 'style')) {
+                const button = document.querySelector('a.missing_vehicles_load');
+                if (button && button.classList.contains('btn-warning') && button.style.display !== 'none') {
+                    clickMissingVehiclesLoad();
+                }
+            }
+        });
+    });
+
+    function clickMissingVehiclesLoad() {
+        const button = document.querySelector('a.missing_vehicles_load');
+        if (button && !button.disabled && clickCount < numberOfTimes) {
+            button.click();
+            clickCount++;
+        }
+    }
+
+    // Begin met observeren
+    const config = { attributes: true, attributeFilter: ['class', 'style'], childList: false, subtree: false };
+    const targetNode = document.querySelector('a.missing_vehicles_load');
+    if (targetNode) {
+        observer.observe(targetNode, config);
+        clickMissingVehiclesLoad(); // Voer de functie de eerste keer uit
+    }
+})();
